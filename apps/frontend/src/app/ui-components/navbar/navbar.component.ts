3197f8b52572181a47b6dba375f1b9e0f5ce1ea7
Tim U <tim@usnbnz.de>
2025-03-31 22:39:31 +0200

    added link reactive navbar

--- apps/frontend/src/app/ui-components/navbar/navbar.component.ts
+++ apps/frontend/src/app/ui-components/navbar/navbar.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  imports: [RouterLink, RouterLinkActive],
 })
 export class NavbarComponent {}
