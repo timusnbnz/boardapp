aac475779d9171cda037f4dead2e7c8adc3d4b04
Tim U <tim@usnbnz.de>
2025-03-31 20:14:45 +0200

    added components

--- apps/frontend/src/app/app.module.ts
+++ apps/frontend/src/app/app.module.ts
@@ -1,24 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
 import { HeaderComponent } from './ui-components/header/header.component';
 import { LoginRegisterComponent } from './ui-components/login-register/login-register.component';
 import { TodoViewComponent } from './ui-components/todo-view/todo-view.component';
 import { KanbanBoardComponent } from './ui-components/kanban-board/kanban-board.component';
 import { TaskFormComponent } from './ui-components/task-form/task-form.component';
 
-@Component({
-  imports: [
-    RouterModule,
+@NgModule({
+  declarations: [
+    AppComponent,
     HeaderComponent,
     LoginRegisterComponent,
     TodoViewComponent,
     KanbanBoardComponent,
     TaskFormComponent
   ],
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppComponent {
-  title = 'frontend';
-}
+export class AppModule {}
---
+++ apps/frontend/src/app/ui-components/kanban-board/kanban-board.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kanban-board',
+  templateUrl: './kanban-board.component.html',
+  styleUrls: ['./kanban-board.component.css']
+})
+export class KanbanBoardComponent {}
