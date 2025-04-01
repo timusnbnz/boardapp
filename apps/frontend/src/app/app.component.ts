299ad288fca3a19b5dd29007e2db459a125cebfb
Tim U <tim@usnbnz.de>
2025-04-02 00:58:05 +0200

    added basic page structure

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,20 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NavbarComponent } from "./ui-components/navbar/navbar.component";
-import { TaskFormComponent } from "./ui-components/task-form/task-form.component";
-import { LoginRegisterComponent } from "./ui-components/login-register/login-register.component";
-import { TodoViewComponent } from "./ui-components/todo-view/todo-view.component";
-import { BoardComponent } from "./ui-components/board/board.component";
 
 @Component({
   imports: [
     RouterModule,
-    NavbarComponent,
-    TaskFormComponent,
-    LoginRegisterComponent,
-    TodoViewComponent,
-    BoardComponent
-  ],
+    NavbarComponent
+],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
