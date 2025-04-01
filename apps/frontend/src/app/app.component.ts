4b512ab7b788a3ae5e932d1c111f6b28382ae24b
Tim U <tim@usnbnz.de>
2025-04-02 00:25:17 +0200

    fixed app root

--- apps/frontend/src/app/app.component.ts
+++ apps/frontend/src/app/app.component.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { LoginRegisterComponent } from './ui-components/login-register/login-register.component';
-import { TodoViewComponent } from './ui-components/todo-view/todo-view.component';
-import { BoardComponent } from './ui-components/board/board.component';
-import { TaskFormComponent } from './ui-components/task-form/task-form.component';
-import { NavbarComponent } from './ui-components/navbar/navbar.component';
+import { NavbarComponent } from "./ui-components/navbar/navbar.component";
+import { TaskFormComponent } from "./ui-components/task-form/task-form.component";
+import { LoginRegisterComponent } from "./ui-components/login-register/login-register.component";
+import { TodoViewComponent } from "./ui-components/todo-view/todo-view.component";
+import { BoardComponent } from "./ui-components/board/board.component";
 
 @Component({
   imports: [
     RouterModule,
+    NavbarComponent,
+    TaskFormComponent,
     LoginRegisterComponent,
     TodoViewComponent,
-    BoardComponent,
-    TaskFormComponent,
-    NavbarComponent
+    BoardComponent
   ],
   selector: 'app-root',
   templateUrl: './app.component.html',
