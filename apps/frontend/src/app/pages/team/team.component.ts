c01a844ed243bae2ea99668b75fb93cc46ab25a5
Tim U <tim@usnbnz.de>
2025-04-07 23:43:05 +0200

    added team component

---
+++ apps/frontend/src/app/pages/team/team.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'page-team',
+  imports: [],
+  templateUrl: './team.component.html',
+  styleUrl: './team.component.css'
+})
+export class TeamComponent {
+
+}
