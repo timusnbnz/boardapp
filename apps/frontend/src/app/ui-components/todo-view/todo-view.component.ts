dd92bec6ec0d94f6a089f249e9d510942e3a84e4
Tim U <tim@usnbnz.de>
2025-04-07 23:41:15 +0200

    removed unused components

--- apps/frontend/src/app/ui-components/todo-view/todo-view.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-todo-view',
-  templateUrl: './todo-view.component.html',
-  styleUrls: ['./todo-view.component.css']
-})
-export class TodoViewComponent {}
