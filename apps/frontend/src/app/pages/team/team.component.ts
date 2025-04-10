13439ffe64b9404865b07b0aace9fda541e50b69
Tim U <tim@usnbnz.de>
2025-04-10 22:18:43 +0200

    added mockup team page

--- apps/frontend/src/app/pages/team/team.component.ts
+++ apps/frontend/src/app/pages/team/team.component.ts
@@ -1,11 +1,16 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'page-team',
-  imports: [],
+  imports: [NgFor],
   templateUrl: './team.component.html',
   styleUrl: './team.component.css'
 })
 export class TeamComponent {
-
+  teamMembers = [
+    { name: 'John Brown', age: 45, address: 'New York No. 1 Lake Park' },
+    { name: 'Jim Green', age: 27, address: 'London No. 1 Lake Park' },
+    { name: 'Joe Black', age: 31, address: 'Sidney No. 1 Lake Park' },
+  ];
 }
