0075480847f6859f0374f5ddb0f9a25f37209fd4
Tim U <tim@usnbnz.de>
2025-04-10 13:45:33 +0200

    added placeholder components

---
+++ apps/frontend/src/app/ui-components/boardstatistics/boardstatistics.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ui-boardstatistics',
+  imports: [],
+  templateUrl: './boardstatistics.component.html',
+  styleUrl: './boardstatistics.component.css'
+})
+export class BoardstatisticsComponent {
+
+}
---
+++ apps/frontend/src/app/ui-components/velocitygraph/velocitygraph.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ui-velocitygraph',
+  imports: [],
+  templateUrl: './velocitygraph.component.html',
+  styleUrl: './velocitygraph.component.css'
+})
+export class VelocitygraphComponent {
+
+}
